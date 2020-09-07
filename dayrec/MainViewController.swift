e497735f01a38a7f2f60cff84c920c6b562d55a7
SOON WON KA <shawnka89@gmail.com>
2020-09-07 17:48:42 +0900

    init

---
+++ dayrec/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  dayrec
+//
+//  Created by SOON WON KA on 2020/09/07.
+//  Copyright © 2020 Shawn. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
